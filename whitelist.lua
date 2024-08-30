4018afccfed25379621d459131f5c46ff225d304
hoholwaredevz <gaevskiivlad703@gmail.com>
2024-08-30 16:57:53 +0300

    Update whitelist

--- whitelist.lua
+++ whitelist.lua
@@ -2,4 +2,5 @@ return {
     [1] = {"pop30e", "QgHSeHVI2QV5jRLu"};
     [2] = {"t3st24_", "Av2dPFRXTDPnmR8p"};
     [3] = {"ronopoldo.", "gLkmsqXnc54gCQBB"};
+    [4] = {"kazaksmert", "0Cv0ZsKJbW6k9YrV"};
 }
